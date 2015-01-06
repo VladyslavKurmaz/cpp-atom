0704482c35db32026ee5581734542d5dde7f1939
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2015-01-06 19:54:48 +0200

    old codes has been removed

--- console/pch.cpp
+++ console/pch.cpp
@@ -42,98 +42,67 @@ std::string gen_uuid() {
 }
 
 /*
-
-
-
-
-	void window::onKey( HWND hWnd, UINT vk, BOOL down, int repeat, UINT flags ){
-
-
-}
-
-
-
-void window::onPaint( HWND hWnd ){
-	struct context {
-		HWND			wnd;
-		PAINTSTRUCT 	ps; 
-		HDC 			dc; 
-		RECT 			rect;
-		paint_param_t&	pp;
-		frame_ptr		current;
-		bool			focused;
-
-		context( HWND w, window& pw ) : pp( pw.paint_param ), wnd( w ) {
-			dc = BeginPaint( wnd, &ps );
-			GetClientRect( wnd, &rect );
-			current		= pw.current_frame;
-			focused		= ( GetFocus() == wnd );
-			//
-			FillRect( pp.mem_dc, &rect, pp.bk_brush );
-		}
-		~context() {
-			
-			EndPaint( wnd, &ps );
-		}
-	} c( hWnd, *this );
-	//
-	
-}
-
-
-
-	void window::processUI() {
-	//
-	this->invalidate();
-}
-
 void window::processAutostart() {
 	//Registry.SetValue(@"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run",
 	//          "MyStartUp",
 	//          @"C:\StartUpApp.exe");
 }
 
-void
-window::parseConf( TCHAR const* conf ) {
-	std::string s( conf );
-	//
-	for( size_t i = 0; i < conf_cmds_cnt; ++i ) {
-		if ( ( s == conf_cmds[i].cmd ) && ( conf_cmds[i].id ) ) {
-			SendMessage( this->get_hwnd(), WM_COMMAND, MAKEWPARAM( conf_cmds[i].id, 0 ), 0 );
-			return;
-		}
-	}
-	// check command line options
-	try {
-		this->getPref().parse( std::string( conf ) );
-	}
-	catch ( std::exception& e ) {
-		//send exception description back to the console process
-	}
-}
-
-
-void
-window::closeFrame( frame_id_t const id ) {
-	frame_ptr f = this->head_area->get_by_id( id );
-	if ( f ) {
-		//
-		if ( this->current_frame == f ) {
-			this->expand_mode = false;
-			this->current_frame = this->head_area->find( f )->next();
-			if ( this->current_frame == f ) {
-				this->current_frame = frame_ptr();
-			}
-		}
-		//
-		this->head_area->find( f )->close();
-		f->clear();
-		//
-		// exit application
-		if ( !this->current_frame ) {
-		}
-	}
-	this->invalidate();
-}
-
 */
+
+#if 0
+BOOL WINAPI bridge::handler( DWORD dwCtrlType ) {
+	switch ( dwCtrlType ){
+	case CTRL_C_EVENT:{
+			return TRUE;
+		}
+	case CTRL_BREAK_EVENT:{
+			return TRUE;
+		}
+	case CTRL_CLOSE_EVENT:{
+			return TRUE;
+		}
+	case CTRL_LOGOFF_EVENT:{
+			return TRUE;
+		}
+	case CTRL_SHUTDOWN_EVENT:{
+			return TRUE;
+		}
+	}
+	return FALSE;
+}
+
+
+#include <strsafe.h>
+void ErrorExit(LPTSTR lpszFunction) 
+{ 
+    // Retrieve the system error message for the last-error code
+
+    LPVOID lpMsgBuf;
+    LPVOID lpDisplayBuf;
+    DWORD dw = GetLastError(); 
+
+    FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+        FORMAT_MESSAGE_FROM_SYSTEM |
+        FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL,
+        dw,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPTSTR) &lpMsgBuf,
+        0, NULL );
+
+    // Display the error message and exit the process
+
+    lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT, 
+        (lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)lpszFunction) + 40) * sizeof(TCHAR)); 
+    StringCchPrintf((LPTSTR)lpDisplayBuf, 
+        LocalSize(lpDisplayBuf) / sizeof(TCHAR),
+        TEXT("%s failed with error %d: %s"), 
+        lpszFunction, dw, lpMsgBuf); 
+    MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK); 
+
+    LocalFree(lpMsgBuf);
+    LocalFree(lpDisplayBuf);
+}
+#endif
